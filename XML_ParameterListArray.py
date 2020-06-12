0e458e9e56ac627a25ee46389a6de0925e7e41c3
Chris Anderson <anderson@math.ucla.edu>
2020-06-12 10:20:20 -0700

    Added GPL notice

--- XML_ParameterListArray.py
+++ XML_ParameterListArray.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
+
+#############################################################################
+#                               XML_ParameterListArray.py 
+#
+# Author: C. Anderson
+# Origin date : June 12, 2020 
+#############################################################################
+#
+# Copyright  2020 Chris Anderson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the Lesser GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# For a copy of the GNU General Public License see
+# <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+ 
 import sys
 import os
 from copy import deepcopy
